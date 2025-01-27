2c16cc4b132525860fe9bdb9e210b6ee1c1ec2fc
passakorn <patsakorns@gmail.com>
2025-01-27 12:36:44 +0700

    third

--- lib/edit_screen.dart
+++ lib/edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_flutter_app/student.dart';
 
 class EditScreen extends StatefulWidget {
   final int index;//ค่าคีย์รายการที่ต้องการแก้ไข
@@ -9,8 +10,134 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+  var nameController = TextEditingController();
+  var idController = TextEditingController();
+  var scoreController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    
+
+    var numItem = widget.index;
+    if (numItem >= 0) {
+      idController.text = Student.studentItems[numItem]['id'];
+      nameController.text = Student.studentItems[numItem]['name'];
+      scoreController.text =Student.studentItems[numItem]['score'].toString();
+    }
+
+    return Container(
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(width: 0),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30)
+        )
+      ),
+      child: Column(
+        children: [
+          const SizedBox(height: 20,),
+          TextField(
+            controller: idController,
+            style: const TextStyle(color: Colors.black, fontSize: 16),
+            decoration: const InputDecoration(
+              labelText: 'รหัสนักศึกษา',
+              focusColor: Colors.pink,
+              labelStyle: TextStyle(
+                decorationColor: Colors.red,
+                color: Colors.blue,
+                fontSize: 20
+              ),
+              icon: Icon(Icons.key, color: Colors.blueGrey,size: 30,),
+              fillColor: Colors.blueGrey,
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey,style: BorderStyle.solid),
+              
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.pinkAccent)
+              )
+            ),
+          ),
+          const SizedBox(height: 20,),
+          TextField(
+            controller: nameController,
+            style: const TextStyle(color: Colors.black, fontSize: 16),
+            decoration: const InputDecoration(
+              labelText: 'ชื่อนักศึกษา',
+              focusColor: Colors.pink,
+              labelStyle: TextStyle(
+                decorationColor: Colors.red,
+                color: Colors.blue,
+                fontSize: 20
+              ),
+              icon: Icon(Icons.person_outline, color: Colors.blueGrey,size: 30,),
+              fillColor: Colors.blueGrey,
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey,style: BorderStyle.solid),
+              
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.pinkAccent)
+              )
+            ),
+          ),
+          const SizedBox(height: 20,),
+          TextField(
+            controller: scoreController,
+            style: const TextStyle(color: Colors.black, fontSize: 16),
+            decoration: const InputDecoration(
+              labelText: 'คะแนน',
+              focusColor: Colors.pink,
+              labelStyle: TextStyle(
+                decorationColor: Colors.red,
+                color: Colors.blue,
+                fontSize: 20
+              ),
+              icon: Icon(Icons.score_outlined, color: Colors.blueGrey,size: 30,),
+              fillColor: Colors.blueGrey,
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blueGrey,style: BorderStyle.solid),
+              
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.pinkAccent)
+              )
+            ),
+          ),
+          const SizedBox(height: 30,),
+          buildButton(),
+        ],
+      ),
+    );
+  }
+  Widget buildButton(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: (){
+            Navigator.of(context).pop();
+          }, 
+          child: const Text('ยกเลิก', 
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+        ),
+        const SizedBox(width: 20,),
+        ElevatedButton(
+          onPressed: (){
+            setState(() {
+              Student.studentItems[widget.index]['id']=idController.text;
+              Student.studentItems[widget.index]['name']=nameController.text;
+              Student.studentItems[widget.index]['score']=int.parse(scoreController.text);
+              Student.updateStudentItems();
+            });
+            Navigator.of(context).pop();
+          }, 
+          child: const Text('แก้ไข', 
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+        )
+      ],);
   }
 }
